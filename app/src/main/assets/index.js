c1dff023e135486158ab34f64b4cab825cfc3a61
Daniel Zheng <danielzheng1989@gmail.com>
2019-03-10 04:31:41 +0100

    added game canvas

--- app/src/main/assets/index.js
+++ app/src/main/assets/index.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+console.log("index.js");
+const canvas = document.getElementById("gameCanvas");
+const g = document.getElementById("gameCanvas").getContext("2d");
+console.log(g);
+
+const renderFrame = () => {
+    g.fillStyle = 'green';
+    g.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+window.addEventListener("resize", renderFrame);
+
+renderFrame();
