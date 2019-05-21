bf93fa883173a869715615eab9e9d92cbdffd9d5
Daniel Zheng <danielzheng1989@gmail.com>
2019-05-22 01:52:52 +0200

    add width and height

--- app/src/main/assets/map1.js
+++ app/src/main/assets/map1.js
@@ -6,6 +6,8 @@ if (!window.gameMaps) {
 	const id = "2fed7019-0391-4f41-b60c-704c62772883";
 	const map = {
 		name: "Level 1",
+		width: 6,
+		height: 3,
 		data: `
 ######
 #PB G#
--- app/src/main/assets/map11.js
+++ app/src/main/assets/map11.js
@@ -6,6 +6,8 @@ if (!window.gameMaps) {
 	const id = "e3b33c29-e353-4a43-90cc-09b944769813";
 	const map = {
 		name: "Level 11",
+		width: 10,
+		height: 8,
 		data: `
   #####
   #G  ###
