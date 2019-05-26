c07046f7815efe6e21f0513f855d70559eda9630
Daniel Zheng <danielzheng1989@gmail.com>
2019-05-26 18:19:26 +0200

    add some screenshots publish to app store

--- app/src/main/java/com/delta/sokoban/FullscreenActivity.kt
+++ app/src/main/java/com/delta/sokoban/FullscreenActivity.kt
@@ -1,4 +1,4 @@
-package com.example.sokoban
+package com.delta.sokoban
 
 import android.annotation.SuppressLint
 import android.os.Bundle
@@ -11,7 +11,6 @@ import android.webkit.WebViewClient
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
-import java.io.InputStreamReader
 import java.net.ServerSocket
 
 /**
